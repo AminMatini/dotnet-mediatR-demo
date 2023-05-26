6601f251ad79a4eae952e32addcc31bbb8ed5d95
Amin Matini <Aminmatini78@gmail.com>
2023-05-26 21:35:33 +0330

    feat : add book model

---
+++ MediatR/Demo.Api/Data/Model/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo.Api.Data.Model;
+
+public class Book
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int PublishYear { get; set; }
+
+}
